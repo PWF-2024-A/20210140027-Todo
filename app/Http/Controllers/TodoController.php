ed6462808311cf1c7fd5efaf7f6a2da62f506ca7
alwan.fauzi.ft21@mail.umy.ac.id <alwan.fauzi.ft21@mail.umy.ac.id>
2024-03-23 16:30:09 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use App\Models\clear;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TodoController extends Controller
 {
@@ -12,6 +14,8 @@ class TodoController extends Controller
      */
     public function index()
     {
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
+        dd($todos);
         return view('todo.index');
     }
 
