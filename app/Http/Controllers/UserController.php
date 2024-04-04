28db75494bb666e920699d920c38c535c711c970
alwan.fauzi.ft21@mail.umy.ac.id <alwan.fauzi.ft21@mail.umy.ac.id>
2024-04-04 14:43:43 +0700

    create, read, search data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\clear;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
@@ -12,6 +14,21 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view ('user.index');
+        $search = request('search');
+        if ($search) {
+            $users = User::where(function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            })
+                ->orderBy('name')
+                ->where('id', '!=', '1')
+                ->paginate(20)
+                ->withQueryString();
+        } else {
+            $users = User::where('id', '!=', '1')
+                ->orderBy('name')
+                ->paginate(10);
+        }
+        return view('user.index', compact('users'));
     }
 }
